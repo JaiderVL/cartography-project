304e8f42f49274c6964c1fc0ff3888e51ac6767a
nicolasgonzalezc <nicolas.cubillos28@hotmail.com>
2024-09-28 21:23:24 -0500

    implementacion de msj errors

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service'; // Importa el AuthService
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule,CommonModule],
   templateUrl: './login-page.component.html',
   styleUrls: ['../page.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  errorMessage: string | null = null; // Variable para manejar el mensaje de error
+  isLoading: boolean = false; // Variable para mostrar el indicador de carga
+
 
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
@@ -22,21 +26,33 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
+      this.isLoading = true; // Mostrar el mensaje de carga
       const { email, password } = this.loginForm.value;
       this.authService.login(email, password)
         .then(() => {
+          this.isLoading = false; // Ocultar el mensaje de carga
           console.log('User logged in');
-            this.router.navigate(['/home'], { replaceUrl: true }); // Reemplaza la entrada de historial
+          this.router.navigate(['/home'], { replaceUrl: true }); // Reemplaza la entrada de historial
         })
-        .catch(error => console.error('Login error:', error));
+        .catch(error => {
+          this.isLoading = false; // Ocultar el mensaje de carga
+          console.error('Login error:', error);
+          this.errorMessage = 'Incorrect email or password. Please try again.'; // Establece el mensaje de error
+        });
     }
   }
+
   loginWithGoogle() {
+    this.isLoading = true; // Mostrar el mensaje de carga
     this.authService.loginWithGoogle()
       .then(() => {
+        this.isLoading = false; // Ocultar el mensaje de carga
         console.log('User logged in with Google');
-        this.router.navigate(['/home'], { replaceUrl: true }); // Reemplaza la entrada de historial
+        this.router.navigate(['/home'], { replaceUrl: true });
       })
-      .catch(error => console.error('Google login error:', error));
+      .catch(error => {
+        this.isLoading = false; // Ocultar el mensaje de carga
+        console.error('Google login error:', error);
+      });
   }
 }
