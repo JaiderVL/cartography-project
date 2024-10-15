b2d58bcc80bade0a7046d23a29133545947d3785
jyvergara <jyvergara@ucundinamarca.edu.co>
2024-10-14 21:47:58 -0500

    mejora del calendario, cordenadas, lista de parques

--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collectionData, collection, doc, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 import { Event } from '../models/event.model';
-import { Observable, from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, from, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -16,13 +16,22 @@ export class EventService {
 
   // Obtener todos los eventos
   getEvents(): Observable<Event[]> {
-    return collectionData(this.eventsCollection, { idField: 'id' }) as Observable<Event[]>;
+    return collectionData(this.eventsCollection, { idField: 'id' }).pipe(
+      catchError(error => {
+        console.error('Error al obtener eventos', error);
+        return of([]);
+      })
+    ) as Observable<Event[]>;
   }
 
   // Agregar un evento
   addEvent(event: Event): Observable<void> {
     return from(addDoc(this.eventsCollection, event)).pipe(
-      map(() => {})
+      map(() => {}),
+      catchError(error => {
+        console.error('Error al agregar evento', error);
+        return of();
+      })
     );
   }
 
@@ -30,7 +39,11 @@ export class EventService {
   updateEvent(event: Event): Observable<void> {
     const eventDocRef = doc(this.firestore, `events/${event.id}`);
     return from(updateDoc(eventDocRef, { ...event })).pipe(
-      map(() => {})
+      map(() => {}),
+      catchError(error => {
+        console.error('Error al actualizar evento', error);
+        return of();
+      })
     );
   }
 
@@ -38,7 +51,11 @@ export class EventService {
   deleteEvent(eventId: string): Observable<void> {
     const eventDocRef = doc(this.firestore, `events/${eventId}`);
     return from(deleteDoc(eventDocRef)).pipe(
-      map(() => {})
+      map(() => {}),
+      catchError(error => {
+        console.error('Error al eliminar evento', error);
+        return of();
+      })
     );
   }
 }
