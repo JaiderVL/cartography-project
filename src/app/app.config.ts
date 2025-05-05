1e7e35ea82916311a52471523cb313afb9fe22b3
nicolasgonzalezc <nicolas.cubillos28@hotmail.com>
2025-05-04 23:52:10 -0500

    add firebase performance service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,25 +5,43 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app'; // Firebase imports
 import { provideAuth, getAuth } from '@angular/fire/auth';
-
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments//environment.prod';
-
 import { provideStorage, getStorage } from '@angular/fire/storage';
+import { providePerformance, getPerformance } from '@angular/fire/performance'; 
 
+
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideZoneChangeDetection({ eventCoalescing: true }),
+//     provideRouter(routes),
+//     provideServiceWorker('ngsw-worker.js', {
+//       enabled: !isDevMode(),
+//       registrationStrategy: 'registerWhenStable:30000'
+//     }),
+//     provideFirebaseApp(() => initializeApp(environment.firebase)),
+//     provideAuth(() => getAuth()),
+//     provideFirestore(() => getFirestore()),
+//     provideStorage(() => getStorage()),
+//     providePerformance(() => getPerformance()),
+//   ]
+// };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)), // Firebase primero
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    providePerformance(() => getPerformance()), // Performance después de otros módulos Firebase
+
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()) // Añade esto
-
+    provideAnimationsAsync()
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { initializeApp } from 'firebase/app';
+import { getPerformance } from 'firebase/performance';
+import { environment } from './environments/environment.prod'; // Asegúrate de importar el entorno correcto
+
+// Inicialización explícita de Firebase Performance
+const app = initializeApp(environment.firebase);
+const perf = getPerformance(app);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
